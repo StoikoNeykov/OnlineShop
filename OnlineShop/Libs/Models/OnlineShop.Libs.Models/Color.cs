65387fc7e37be8d6183886f42fb0c76320952381
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-25 13:06:46 +0200

    added EF to models and introduce Color class

---
+++ OnlineShop/Libs/Models/OnlineShop.Libs.Models/Color.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using OnlineShop.Libs.Models.Contracts;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OnlineShop.Libs.Models
+{
+    [Table("Colours")]
+    public class Color : IDbModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public string Name { get; set; }
+
+        public string HexColor { get; set; }
+    }
+}
