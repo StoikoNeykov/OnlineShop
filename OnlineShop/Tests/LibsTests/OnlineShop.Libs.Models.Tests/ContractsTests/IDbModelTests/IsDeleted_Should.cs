67f949ea7782d303aab65b427ce20a086e07ea3f
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-25 16:37:08 +0200

    id tess transform in more generic variant

---
+++ OnlineShop/Tests/LibsTests/OnlineShop.Libs.Models.Tests/ContractsTests/IDbModelTests/IsDeleted_Should.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using OnlineShop.Libs.Models.Contracts;
+using System;
+using System.Linq;
+
+namespace OnlineShop.Libs.Models.Tests.ContractsTests.IDbModelTests
+{
+    [TestFixture]
+    public class IsDeleted_Should
+    {
+        [TestCase(true)]
+        [TestCase(false)]
+        public void GetAndSet_Should_Work(bool value)
+        {
+            var types = AppDomain
+                           .CurrentDomain
+                           .GetAssemblies()
+                           .SelectMany(x => x.GetTypes())
+                           .Where(x => x.IsClass && !x.IsAbstract && x.GetInterfaces().Contains(typeof(IDbModel)));
+
+            foreach(Type type in types)
+            {
+                var obj = (IDbModel)Activator.CreateInstance(type);
+
+                obj.IsDeleted = value;
+
+                Assert.AreEqual(value, obj.IsDeleted);
+            }
+        }
+    }
+}
