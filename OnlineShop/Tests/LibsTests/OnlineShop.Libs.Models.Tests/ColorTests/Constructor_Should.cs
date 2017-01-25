75244716079289b4c8d72ebd72b3dce17031375d
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-25 15:18:15 +0200

    added color constructor tests

---
+++ OnlineShop/Tests/LibsTests/OnlineShop.Libs.Models.Tests/ColorTests/Constructor_Should.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+
+namespace OnlineShop.Libs.Models.Tests.ColorTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void Parameterless_Constructor_Should_Exsist()
+        {
+            var obj = new Color();
+
+            Assert.IsInstanceOf<Color>(obj);
+        }
+
+        [Test]
+        public void NotSet_Id()
+        {
+            var obj = new Color();
+
+            Assert.AreEqual(0, obj.Id);
+        }
+
+        [Test]
+        public void NotSet_IsDeleted()
+        {
+            var obj = new Color();
+
+            Assert.AreEqual(false, obj.IsDeleted);
+        }
+
+        [Test]
+        public void NotSet_Name()
+        {
+            var obj = new Color();
+
+            Assert.AreEqual(null, obj.Name);
+        }
+
+        [Test]
+        public void NotSet_HexCode()
+        {
+            var obj = new Color();
+
+            Assert.AreEqual(null, obj.HexColor);
+        }
+    }
+}
