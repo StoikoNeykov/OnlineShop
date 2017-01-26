783348a2631ed4b3060e5002160cd1c386e23f30
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-26 05:31:52 +0200

    relocate some tests and now more generic

--- OnlineShop/Tests/LibsTests/OnlineShop.Libs.Models.Tests/ColorTests/Constructor_Should.cs
+++ OnlineShop/Tests/LibsTests/OnlineShop.Libs.Models.Tests/ColorTests/Constructor_Should.cs
@@ -6,30 +6,6 @@ namespace OnlineShop.Libs.Models.Tests.ColorTests
     public class Constructor_Should
     {
         [Test]
-        public void Parameterless_Constructor_Should_Exsist()
-        {
-            var obj = new Color();
-
-            Assert.IsInstanceOf<Color>(obj);
-        }
-
-        [Test]
-        public void NotSet_Id()
-        {
-            var obj = new Color();
-
-            Assert.AreEqual(0, obj.Id);
-        }
-
-        [Test]
-        public void NotSet_IsDeleted()
-        {
-            var obj = new Color();
-
-            Assert.AreEqual(false, obj.IsDeleted);
-        }
-
-        [Test]
         public void NotSet_Name()
         {
             var obj = new Color();
---
+++ OnlineShop/Tests/LibsTests/OnlineShop.Libs.Models.Tests/ContractsTests/IDbModelTests/ParameterlessConstructor_Should.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using OnlineShop.Libs.Models.Contracts;
+using System;
+using System.Linq;
+
+namespace OnlineShop.Libs.Models.Tests.ContractsTests.IDbModelTests
+{
+    [TestFixture]
+    public class ParameterlessConstructor_Should
+    {
+
+        [Test]
+        public void Exist()
+        {
+            var types = AppDomain
+                .CurrentDomain
+                .GetAssemblies()
+                .SelectMany(x => x.GetTypes())
+                .Where(x => x.IsClass && !x.IsAbstract && x.GetInterfaces().Contains(typeof(IDbModel)));
+
+            foreach (Type type in types)
+            {
+                var obj = (IDbModel)Activator.CreateInstance(type);
+
+                Assert.IsInstanceOf<IDbModel>(obj);
+            }
+        }
+
+        [Test]
+        public void NotSet_Id()
+        {
+            var types = AppDomain
+                .CurrentDomain
+                .GetAssemblies()
+                .SelectMany(x => x.GetTypes())
+                .Where(x => x.IsClass && !x.IsAbstract && x.GetInterfaces().Contains(typeof(IDbModel)));
+
+            foreach (Type type in types)
+            {
+                var obj = (IDbModel)Activator.CreateInstance(type);
+
+                Assert.AreEqual(0, obj.Id);
+            }
+        }
+
+        [Test]
+        public void NotSet_IsDeleted()
+        {
+            var types = AppDomain
+                .CurrentDomain
+                .GetAssemblies()
+                .SelectMany(x => x.GetTypes())
+                .Where(x => x.IsClass && !x.IsAbstract && x.GetInterfaces().Contains(typeof(IDbModel)));
+
+            foreach (Type type in types)
+            {
+                var obj = (IDbModel)Activator.CreateInstance(type);
+
+                Assert.AreEqual(false, obj.IsDeleted);
+            }
+        }
+    }
+}
