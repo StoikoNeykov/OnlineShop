87e142210112aea977e1a495e89d6734e5a5c295
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-26 06:08:06 +0200

    added country constructor shoul tests

---
+++ OnlineShop/Tests/LibsTests/OnlineShop.Libs.Models.Tests/CountryTests/Construtor_Should.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace OnlineShop.Libs.Models.Tests.CountryTests
+{
+    [TestFixture]
+    public class Construtor_Should
+    {
+        [Test]
+        public void NotSet_Name()
+        {
+            var obj = new Country();
+
+            Assert.AreEqual(null, obj.Name);
+        }
+
+        [Test]
+        public void NotSet_ShortName()
+        {
+            var obj = new Country();
+
+            Assert.AreEqual(null, obj.ShortName);
+        }
+    }
+}
