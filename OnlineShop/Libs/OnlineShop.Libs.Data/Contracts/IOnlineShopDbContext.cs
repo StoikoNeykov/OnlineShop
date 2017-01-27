7a9a63ad34903d58f416fd0ec2d57aafb9d5b4fd
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-27 16:00:11 +0200

    added IDbContext and IStateful interfaces

---
+++ OnlineShop/Libs/OnlineShop.Libs.Data/Contracts/IOnlineShopDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Libs.Data.Contracts
+{
+    public interface IOnlineShopDbContext
+    {
+        DbSet<TEntity> Set<TEntity>() where TEntity : class;
+
+        IStateful<TEntity> GetStateful<TEntity>(TEntity entity) where TEntity : class;
+
+        Task<int> SaveChangesAsync();
+
+        int SaveChanges();
+    }
+}
---
+++ OnlineShop/Libs/OnlineShop.Libs.Data/Contracts/IStateful.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+
+namespace OnlineShop.Libs.Data.Contracts
+{
+    public interface IStateful<T>
+    {
+        EntityState State { get; set; }
+
+    }
+}
