0125a17ed5f1a2a016c63bc21017cca95d2a01b3
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-28 07:38:22 +0200

    added IStateful factory

---
+++ OnlineShop/Libs/OnlineShop.Libs.Data/Factories/IStatefulFactory.cs
@@ -0,0 +1,10 @@
+﻿using OnlineShop.Libs.Data.Contracts;
+using System.Data.Entity.Infrastructure;
+
+namespace OnlineShop.Libs.Data.Factories
+{
+    public interface IStatefulFactory
+    {
+        IStateful<T> GetStateful<T>(DbEntityEntry<T> entity) where T : class;
+    }
+}
