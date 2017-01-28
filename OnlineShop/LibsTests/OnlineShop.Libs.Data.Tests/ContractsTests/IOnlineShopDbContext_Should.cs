0295c5266aa24a019103afbd9a204085320f55c2
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-28 08:53:06 +0200

    added test that secure all IDbModel classes are registered in DbContexts

---
+++ OnlineShop/LibsTests/OnlineShop.Libs.Data.Tests/ContractsTests/IOnlineShopDbContext_Should.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using OnlineShop.Libs.Data.Contracts;
+using OnlineShop.Libs.Models.Contracts;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace OnlineShop.Libs.Data.Tests.ContractsTests
+{
+    [TestFixture]
+    public class IOnlineShopDbContext_Should
+    {
+        [Test]
+        public void HaveSetsFor_AllDbModels()
+        {
+            var modelsAssemblyName = "OnlineShop.Libs.Models";
+
+            var expected = Assembly.Load(modelsAssemblyName)
+                                    .GetTypes()
+                                    .Where(x => x.IsClass && !x.IsAbstract && x.GetInterfaces().Contains(typeof(IDbModel)));
+            ;
+
+            var result = typeof(IOnlineShopDbContext)
+                                .GetProperties()
+                                .Select(x => x.PropertyType
+                                            .GetGenericArguments()
+                                            .SingleOrDefault());
+            
+            CollectionAssert.AreEquivalent(expected, result);
+        }
+    }
+}
