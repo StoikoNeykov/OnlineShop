c94b19a907086fb6fe957bac434ec9b00b9d3c69
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-29 08:32:27 +0200

    added unitOfWork SaveChangesAsync should tests

---
+++ OnlineShop/Tests/LibsTests/OnlineShop.Libs.Data.Tests/UnitOfWorkTests/SaveChangesAsync_Should.cs
@@ -0,0 +1,22 @@
+﻿using Moq;
+using NUnit.Framework;
+using OnlineShop.Libs.Data.Contracts;
+
+namespace OnlineShop.Libs.Data.Tests.UnitOfWorkTests
+{
+    [TestFixture]
+    public class SaveChangesAsync_Should
+    {
+        [Test]
+        public void Call_DbContext_SaveChangesAsync()
+        {
+            var mockedContext = new Mock<IOnlineShopDbContext>();
+            mockedContext.Setup(x => x.SaveChangesAsync()).Verifiable();
+
+            var obj = new UnitOfWork(mockedContext.Object);
+            obj.SaveChangesAsync();
+
+            mockedContext.Verify(x => x.SaveChangesAsync(), Times.Once);
+        }
+    }
+}
