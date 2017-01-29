06976d0677561fe57ee6a87d8a55d32d91aaff78
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-29 07:33:39 +0200

    added unit of work

---
+++ OnlineShop/Libs/OnlineShop.Libs.Data/UnitOfWork.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using OnlineShop.Libs.Data.Contracts;
+
+namespace OnlineShop.Libs.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private IOnlineShopDbContext dbContext;
+
+        public UnitOfWork(IOnlineShopDbContext dbContext)
+        {
+            if (dbContext == null)
+            {
+                throw new ArgumentNullException("OnlineShopDbContext");
+            }
+
+            this.dbContext = dbContext;
+        }
+
+        public int SaveChanges()
+        {
+            return this.dbContext.SaveChanges();
+        }
+
+        public Task<int> SaveChangesAsync()
+        {
+            return this.dbContext.SaveChangesAsync();
+        }
+    }
+}
