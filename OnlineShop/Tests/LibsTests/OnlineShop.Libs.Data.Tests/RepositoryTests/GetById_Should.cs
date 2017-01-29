62f8fac9f505f7fa95b25ef06e586365ad8887fb
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-29 11:24:20 +0200

    added repo GetById should tests

---
+++ OnlineShop/Tests/LibsTests/OnlineShop.Libs.Data.Tests/RepositoryTests/GetById_Should.cs
@@ -0,0 +1,32 @@
+﻿using Moq;
+using NUnit.Framework;
+using OnlineShop.Libs.Data.Contracts;
+using OnlineShop.Libs.Data.Tests.Mocks;
+using System.Data.Entity;
+using System.Linq;
+
+namespace OnlineShop.Libs.Data.Tests.RepositoryTests
+{
+    [TestFixture]
+    public class GetById_Should
+    {
+        [TestCase(5)]
+        [TestCase(3)]
+        [TestCase(21)]
+        [TestCase(6)]
+        public void Should_Work(int randomId)
+        {
+            var mockedSet = new Mock<IDbSet<DimmyClass>>();
+            mockedSet.Setup(x => x.Find(randomId)).Verifiable();
+
+            var mockedContext = new Mock<IOnlineShopDbContext>();
+            mockedContext.Setup(x => x.Set<DimmyClass>()).Returns(mockedSet.Object);
+
+            var obj = new Repository<DimmyClass>(mockedContext.Object);
+
+            var actual = obj.GetById(randomId);
+
+            mockedSet.Verify();
+        }
+    }
+}
