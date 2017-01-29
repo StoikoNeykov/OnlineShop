407bc048ea446ea6b6f48a0788efe4d32898da15
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-29 09:55:39 +0200

    added repository class should tests

---
+++ OnlineShop/Tests/LibsTests/OnlineShop.Libs.Data.Tests/RepositoryTests/Class_Should.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+using OnlineShop.Libs.Models.Contracts;
+using OnlineShop.Libs.Data.Contracts;
+using System.Linq;
+
+namespace OnlineShop.Libs.Data.Tests.RepositoryTests
+{
+    [TestFixture]
+    public class Class_Should
+    {
+        [Test]
+        public void Implement_Default_Interface()
+        {
+            var result = typeof(Repository<IDbModel>)
+                                .GetInterfaces()
+                                .SingleOrDefault(x => x == typeof(IRepository<IDbModel>));
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
