ad4d169cafdc8c266f4b51238e24d08d6766e7fe
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-29 09:06:00 +0200

    added Stateful constructor should tests

---
+++ OnlineShop/Tests/LibsTests/OnlineShop.Libs.Data.Tests/StatefulTests/Constructor_Should.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using OnlineShop.Libs.Data;
+
+namespace OnlineShop.Libs.Data.Tests.StatefulTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void Throw_ArgumentNullException_WithProperMessage_WhenEntry_IsNull()
+        {
+            Assert.That(() => new Stateful<DimmyClass>(null),
+                                Throws.ArgumentNullException.With.Message.Contains("Entry"));
+        }
+        
+        private class DimmyClass { }
+    }
+}
