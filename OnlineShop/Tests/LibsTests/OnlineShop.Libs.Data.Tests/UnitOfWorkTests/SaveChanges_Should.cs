1b45ffa5b531bdba5a436a0189935fa7f54a2001
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-29 08:28:37 +0200

    aded unitOfWork SaveChanges should tests

---
+++ OnlineShop/Tests/LibsTests/OnlineShop.Libs.Data.Tests/UnitOfWorkTests/SaveChanges_Should.cs
@@ -0,0 +1,22 @@
+﻿using Moq;
+using NUnit.Framework;
+using OnlineShop.Libs.Data.Contracts;
+
+namespace OnlineShop.Libs.Data.Tests.UnitOfWorkTests
+{
+    [TestFixture]
+    public class SaveChanges_Should
+    {
+        [Test]
+        public void Call_DbContext_SaveChanges()
+        {
+            var mockedContext = new Mock<IOnlineShopDbContext>();
+            mockedContext.Setup(x => x.SaveChanges()).Verifiable();
+
+            var obj = new UnitOfWork(mockedContext.Object);
+            obj.SaveChanges();
+
+            mockedContext.Verify(x => x.SaveChanges(), Times.Once);
+        }
+    }
+}
