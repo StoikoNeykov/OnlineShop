d46514bb2e781384db8072b6256715ca9145b26a
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-29 11:16:00 +0200

    added repo GetDeleted should tests

--- OnlineShop/Tests/LibsTests/OnlineShop.Libs.Data.Tests/Mocks/DimmyClass.cs
+++ OnlineShop/Tests/LibsTests/OnlineShop.Libs.Data.Tests/Mocks/DimmyClass.cs
@@ -1,19 +1,46 @@
 ﻿using OnlineShop.Libs.Models.Contracts;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace OnlineShop.Libs.Data.Tests.Mocks
 {
+    // for test purpose only
+
     public class DimmyClass : IDbModel
     {
-        public int Id
-        {
-            get { return 42; }
-            set { }
-        }
+        public int Id { get; set; }
 
-        public bool IsDeleted
+        public bool IsDeleted { get; set; }
+
+        public static IEnumerable<DimmyClass> GetDimmyCollection()
         {
-            get { return true; }
-            set { }
+            return new List<DimmyClass>()
+            {
+                new DimmyClass {Id = 11, IsDeleted = true },
+                new DimmyClass {Id = 1, IsDeleted = false },
+                new DimmyClass {Id = 6, IsDeleted = true },
+                new DimmyClass {Id = 5, IsDeleted = true },
+                new DimmyClass {Id = 0, IsDeleted = false },
+                new DimmyClass {Id = 12, IsDeleted = false },
+                new DimmyClass {Id = 14, IsDeleted = false },
+                new DimmyClass {Id = 21, IsDeleted = true },
+                new DimmyClass {Id = 20, IsDeleted = true },
+                new DimmyClass {Id = 19, IsDeleted = false },
+                new DimmyClass {Id = 17, IsDeleted = false },
+                new DimmyClass {Id = 18, IsDeleted = false },
+                new DimmyClass {Id = 8, IsDeleted = true },
+                new DimmyClass {Id = 22, IsDeleted = false },
+                new DimmyClass {Id = 9, IsDeleted = false },
+                new DimmyClass {Id = 16, IsDeleted = false },
+                new DimmyClass {Id = 3, IsDeleted = false },
+                new DimmyClass {Id = 15, IsDeleted = true },
+                new DimmyClass {Id = 7, IsDeleted = false },
+                new DimmyClass {Id = 23, IsDeleted = true },
+                new DimmyClass {Id = 10, IsDeleted = false },
+                new DimmyClass {Id = 13, IsDeleted = true },
+                new DimmyClass {Id = 2, IsDeleted = false }
+            };
         }
     }
 }
---
+++ OnlineShop/Tests/LibsTests/OnlineShop.Libs.Data.Tests/RepositoryTests/GetDeleted_Should.cs
@@ -0,0 +1,37 @@
+﻿using Moq;
+using NUnit.Framework;
+using OnlineShop.Libs.Data.Contracts;
+using OnlineShop.Libs.Data.Tests.Helpers;
+using OnlineShop.Libs.Data.Tests.Mocks;
+using System.Data.Entity;
+using System.Linq;
+
+namespace OnlineShop.Libs.Data.Tests.RepositoryTests
+{
+    [TestFixture]
+    public class GetDeleted_Should
+    {
+        [Test]
+        public void Return_DeletedMembers_FromSet_Only()
+        {
+            var expected = DimmyClass
+                                .GetDimmyCollection()
+                                .Where(x => x.IsDeleted)
+                                .Select(x => x.Id)
+                                .ToList();
+
+            var mockedSetObject = QueryableDbSetMock.GetQueryableMockDbSet(DimmyClass.GetDimmyCollection());
+
+            var mockedContext = new Mock<IOnlineShopDbContext>();
+            mockedContext.Setup(x => x.Set<DimmyClass>()).Returns(mockedSetObject);
+
+            var obj = new Repository<DimmyClass>(mockedContext.Object);
+
+            var actual = obj
+                            .GetDeleted()
+                            .Select(x => x.Id);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
