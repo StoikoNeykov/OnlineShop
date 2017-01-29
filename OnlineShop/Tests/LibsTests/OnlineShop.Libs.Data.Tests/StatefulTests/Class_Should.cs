ac8e53943db184f4e78d94989318dfb8a0856e97
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-29 09:01:02 +0200

    added Stateful class should tests

---
+++ OnlineShop/Tests/LibsTests/OnlineShop.Libs.Data.Tests/StatefulTests/Class_Should.cs
@@ -0,0 +1,24 @@
+﻿using Moq;
+using NUnit.Framework;
+using OnlineShop.Libs.Data.Contracts;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+
+namespace OnlineShop.Libs.Data.Tests.StatefulTests
+{
+    [TestFixture]
+    public class Class_Should
+    {
+        [Test]
+        public void Implement_Default_Interface()
+        {
+            var result = typeof(Stateful<DimmyClass>)
+                            .GetInterfaces()
+                            .SingleOrDefault(x => x == typeof(IStateful<DimmyClass>));
+
+            Assert.IsNotNull(result);
+        }
+
+        private class DimmyClass { }
+    }
+}
