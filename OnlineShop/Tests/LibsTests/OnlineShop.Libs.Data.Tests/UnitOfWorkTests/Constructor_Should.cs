4a9e358e8221533f54c02dd7fdbdcb3904c43b1e
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-29 08:22:22 +0200

    added unitOfWork constructor should tests

---
+++ OnlineShop/Tests/LibsTests/OnlineShop.Libs.Data.Tests/UnitOfWorkTests/Constructor_Should.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace OnlineShop.Libs.Data.Tests.UnitOfWorkTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void Throw_NullReferenceException_WithProperMessage_WhenDbContext_IsNull()
+        {
+            Assert.That(() => new UnitOfWork(null),
+                                Throws.ArgumentNullException.With.Message.Contains("DbContext"));
+        }
+    }
+}
