ac4412ffd8f5141da66a244a2ea1f65ae0a86fa5
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-29 10:26:41 +0200

    added repository update should tests

---
+++ OnlineShop/Tests/LibsTests/OnlineShop.Libs.Data.Tests/RepositoryTests/Update_Should.cs
@@ -0,0 +1,47 @@
+﻿using Moq;
+using NUnit.Framework;
+using OnlineShop.Libs.Data.Contracts;
+using OnlineShop.Libs.Data.Tests.Mocks;
+using System.Data.Entity;
+
+namespace OnlineShop.Libs.Data.Tests.RepositoryTests
+{
+    [TestFixture]
+    public class Update_Should
+    {
+        [Test]
+        public void Throw_ArgumentNullException_WithProperMessage_WhenEntity_IsNull()
+        {
+            var mockedSet = new Mock<IDbSet<DimmyClass>>();
+            var mockedStateful = new Mock<IStateful<DimmyClass>>();
+
+            var mockedContext = new Mock<IOnlineShopDbContext>();
+            mockedContext.Setup(x => x.Set<DimmyClass>()).Returns(mockedSet.Object);
+            mockedContext.Setup(x => x.GetStateful<DimmyClass>(It.IsAny<DimmyClass>())).Returns(mockedStateful.Object);
+
+            var obj = new Repository<DimmyClass>(mockedContext.Object);
+
+            Assert.That(() => obj.Update(null),
+                                   Throws.ArgumentNullException.With.Message.Contains("Entity"));
+        }
+
+        [Test]
+        public void Change_StatefulState_ToModified()
+        {
+            var mockedSet = new Mock<IDbSet<DimmyClass>>();
+
+            var mockedStateful = new Mock<IStateful<DimmyClass>>();
+            mockedStateful.SetupSet(x => x.State = EntityState.Modified).Verifiable();
+
+            var mockedContext = new Mock<IOnlineShopDbContext>();
+            mockedContext.Setup(x => x.Set<DimmyClass>()).Returns(mockedSet.Object);
+            mockedContext.Setup(x => x.GetStateful<DimmyClass>(It.IsAny<DimmyClass>())).Returns(mockedStateful.Object);
+
+            var obj = new Repository<DimmyClass>(mockedContext.Object);
+
+            obj.Update(new DimmyClass());
+
+            mockedStateful.Verify();
+        }
+    }
+}
