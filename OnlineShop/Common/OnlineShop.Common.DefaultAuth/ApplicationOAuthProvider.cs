aa55d1f2ce82bfb4531793021c8d6c194edbdbdc
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-30 15:34:13 +0200

    moved ApplicationOAuthProvider

--- OnlineShop/Common/OnlineShop.Common.DefaultAuth/ApplicationOAuthProvider.cs
+++ OnlineShop/Common/OnlineShop.Common.DefaultAuth/ApplicationOAuthProvider.cs
@@ -1,17 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
 using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
-using Microsoft.AspNet.Identity.Owin;
-using Microsoft.Owin.Security;
-using Microsoft.Owin.Security.Cookies;
-using Microsoft.Owin.Security.OAuth;
-using OnlineShop.WebApi.Models;
 
-namespace OnlineShop.WebApi.Providers
+namespace OnlineShop.Common.DefaultAuth
 {
     public class ApplicationOAuthProvider : OAuthAuthorizationServerProvider
     {
