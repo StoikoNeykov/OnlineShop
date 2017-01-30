555892d2f251b3549e2607fcc93b9e3a392e5933
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-30 15:30:16 +0200

    moved ApplicationDbContext

--- OnlineShop/Common/OnlineShop.Common.DefaultAuth/ApplicationDbContext.cs
+++ OnlineShop/Common/OnlineShop.Common.DefaultAuth/ApplicationDbContext.cs
@@ -1,20 +1,18 @@
-﻿using System.Security.Claims;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
-using Microsoft.AspNet.Identity.Owin;
 
-namespace OnlineShop.WebApi.Models
+namespace OnlineShop.Common.DefaultAuth
 {
-
-
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
-        
+
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
