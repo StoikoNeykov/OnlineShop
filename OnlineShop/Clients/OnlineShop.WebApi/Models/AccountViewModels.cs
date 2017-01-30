d46de7bac739bbff67b8f5e96dc29cf8dc66fdf8
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-30 15:55:57 +0200

    moved ExternalLoginViewModel

--- OnlineShop/Clients/OnlineShop.WebApi/Models/AccountViewModels.cs
+++ OnlineShop/Clients/OnlineShop.WebApi/Models/AccountViewModels.cs
@@ -5,14 +5,7 @@ namespace OnlineShop.WebApi.Models
 {
     // Models returned by AccountController actions.
 
-    public class ExternalLoginViewModel
-    {
-        public string Name { get; set; }
 
-        public string Url { get; set; }
-
-        public string State { get; set; }
-    }
 
     public class ManageInfoViewModel
     {
---
+++ OnlineShop/Common/OnlineShop.Common.DefaultAuth/AccountViewModels/ExternalLoginViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace OnlineShop.Common.DefaultAuth.AccountViewModels
+{
+    public class ExternalLoginViewModel
+    {
+        public string Name { get; set; }
+
+        public string Url { get; set; }
+
+        public string State { get; set; }
+    }
+}
