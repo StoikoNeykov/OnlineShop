5cba569ffe6c63f83d3726bb5560532d3f789a38
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-30 15:53:16 +0200

    moved RemoveLoginBindingModel

--- OnlineShop/Clients/OnlineShop.WebApi/Models/AccountBindingModels.cs
+++ OnlineShop/Clients/OnlineShop.WebApi/Models/AccountBindingModels.cs
@@ -14,16 +14,7 @@ namespace OnlineShop.WebApi.Models
 
 
 
-    public class RemoveLoginBindingModel
-    {
-        [Required]
-        [Display(Name = "Login provider")]
-        public string LoginProvider { get; set; }
 
-        [Required]
-        [Display(Name = "Provider key")]
-        public string ProviderKey { get; set; }
-    }
 
     public class SetPasswordBindingModel
     {
---
+++ OnlineShop/Common/OnlineShop.Common.DefaultAuth/AccountBindingModels/RemoveLoginBindingModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShop.Common.DefaultAuth.AccountBindingModels
+{
+    public class RemoveLoginBindingModel
+    {
+        [Required]
+        [Display(Name = "Login provider")]
+        public string LoginProvider { get; set; }
+
+        [Required]
+        [Display(Name = "Provider key")]
+        public string ProviderKey { get; set; }
+    }
+}
