d872629680c83f62f6561c48589a2684630fa71e
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-30 15:56:47 +0200

    moved ManageInfoViewModel

--- OnlineShop/Clients/OnlineShop.WebApi/Models/AccountViewModels.cs
+++ OnlineShop/Clients/OnlineShop.WebApi/Models/AccountViewModels.cs
@@ -7,16 +7,7 @@ namespace OnlineShop.WebApi.Models
 
 
 
-    public class ManageInfoViewModel
-    {
-        public string LocalLoginProvider { get; set; }
 
-        public string Email { get; set; }
-
-        public IEnumerable<UserLoginInfoViewModel> Logins { get; set; }
-
-        public IEnumerable<ExternalLoginViewModel> ExternalLoginProviders { get; set; }
-    }
 
     public class UserInfoViewModel
     {
