665b43887ecb0f8dae8b224961dfb2defae64713
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-30 15:49:08 +0200

    move AddExternalLoginBindingModel

--- OnlineShop/Clients/OnlineShop.WebApi/Models/AccountBindingModels.cs
+++ OnlineShop/Clients/OnlineShop.WebApi/Models/AccountBindingModels.cs
@@ -6,12 +6,7 @@ namespace OnlineShop.WebApi.Models
 {
     // Models used as parameters to AccountController actions.
 
-    public class AddExternalLoginBindingModel
-    {
-        [Required]
-        [Display(Name = "External access token")]
-        public string ExternalAccessToken { get; set; }
-    }
+
 
     public class ChangePasswordBindingModel
     {
---
+++ OnlineShop/Common/OnlineShop.Common.DefaultAuth/AccountBindingModels/AddExternalLoginBindingModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShop.Common.DefaultAuth.AccountBindingModels
+{
+    public class AddExternalLoginBindingModel
+    {
+        [Required]
+        [Display(Name = "External access token")]
+        public string ExternalAccessToken { get; set; }
+    }
+}
