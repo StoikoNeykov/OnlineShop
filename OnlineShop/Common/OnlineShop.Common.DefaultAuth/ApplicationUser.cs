63ba43b57253a4431804b1de1510120f781c268f
Stoiko Neykov <stoikoneykov@gmail.com>
2017-01-30 15:28:46 +0200

    moved ApplicationUser

---
+++ OnlineShop/Common/OnlineShop.Common.DefaultAuth/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace OnlineShop.Common.DefaultAuth
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+}
