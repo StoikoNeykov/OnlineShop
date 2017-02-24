61344ea6bc1f00003e28e5265d97043982c130ef
Stoiko Neykov <stoikoneykov@gmail.com>
2017-02-24 08:01:12 +0200

    added BaseService UnitOfWorkFactory_Should tests

---
+++ OnlineShop/OnlineShop.Services.Tests/AbstractionTests/BaseServiceTests/UnitOfWorkFactory_Should.cs
@@ -0,0 +1,24 @@
+﻿using Moq;
+using NUnit.Framework;
+using OnlineShop.Libs.Data.Factories;
+using OnlineShop.Services.Tests.AbstractionTests.BaseServiceTests.Mock;
+
+namespace OnlineShop.Services.Tests.AbstractionTests.BaseServiceTests
+{
+    [TestFixture]
+    public class UnitOfWorkFactory_Should
+    {
+        [Test]
+        public void Return_SameFactory_PassedInConstructor()
+        {
+            // Arange
+            var mockedFactory = new Mock<IUnitOfWorkFactory>();
+
+            // Act 
+            var obj = new ServiceChild(mockedFactory.Object);
+            
+            // Assert
+            Assert.AreSame(mockedFactory.Object, obj.UnitOfWorkFactory);
+        }
+    }
+}
