7562bd6fc8c8953f63379708d91143cb7452e5d3
Stoiko Neykov <stoikoneykov@gmail.com>
2017-03-10 18:04:36 +0200

    added some ninject modules

---
+++ OnlineShop/Configuration/OnlineShop.Configuration.NinjectConfigs/ConfigurationProvidersNinJectModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Ninject.Modules;
+using OnlineShop.ConfigurationProviders.Contracts;
+using OnlineShop.ConfigurationProviders;
+using Ninject.Web.Common;
+
+namespace OnlineShop.Configuration.NinjectConfigs
+{
+    public class ConfigurationProvidersNinJectModule : NinjectModule
+    {
+        public override void Load()
+        {
+            this.Kernel.Bind<IConnectionStringProvider>().To<ConnectionStringProvider>().InRequestScope();
+            this.Kernel.Bind<IEnvoirmentProvider>().To<EnvoirmentProvider>().InRequestScope();
+        }
+    }
+}
---
+++ OnlineShop/Configuration/OnlineShop.Configuration.NinjectConfigs/DataNinjectModule.cs
@@ -0,0 +1,50 @@
+﻿using Ninject.Modules;
+using OnlineShop.Libs.Data.Contracts;
+using OnlineShop.Libs.Data;
+using Ninject.Web.Common;
+using Ninject;
+using OnlineShop.ConfigurationProviders.Contracts;
+using Ninject.Parameters;
+using Ninject.Extensions.Factory;
+using OnlineShop.Libs.Data.Factories;
+using OnlineShop.Libs.Models.Contracts;
+
+namespace OnlineShop.Configuration.NinjectConfigs
+{
+    public class DataNinjectModule : NinjectModule
+    {
+        public override void Load()
+        {
+            this.Kernel.Bind<EfOnlineShopDbContext>().ToSelf().InRequestScope();
+
+            this.Kernel.Bind(typeof(IEfOnlineShopDbContext),
+                                typeof(IEfEntryProvider),
+                                typeof(IEfUnitOfWork))
+                                .ToMethod(ctx =>
+                                {
+                                    var connectionString = ctx.Kernel.Get<IConnectionStringProvider>().ConnectionString;
+
+                                    var ctorArg = new ConstructorArgument("connectionString", connectionString);
+
+                                    var db = ctx.Kernel.Get<EfOnlineShopDbContext>(ctorArg);
+
+                                    return db;
+                                }).InRequestScope();
+
+
+
+            this.Kernel.Bind(typeof(IEfQuerable<>)).ToMethod(ctx =>
+            {
+                var type = ctx.GenericArguments[0];
+
+                var dbSet = ctx.Kernel.Get<EfOnlineShopDbContext>().Set(type);
+
+                var ctorArgs = new ConstructorArgument("dbSet", dbSet);
+
+                var efQuerable = ctx.Kernel.Get(typeof(EfQuerable<>), ctorArgs);
+
+                return efQuerable;
+            }).InRequestScope().NamedLikeFactoryMethod((IEfQuerableFactory fac)=>fac.GetQuerable<IDbModel>(null));
+        }
+    }
+}
