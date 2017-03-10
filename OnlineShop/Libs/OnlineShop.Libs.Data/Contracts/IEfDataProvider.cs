6417ee6d8a506e56fa2483961bd82b4da0f42a1d
Stoiko Neykov <stoikoneykov@gmail.com>
2017-03-10 10:21:33 +0200

    Introduce IEfDataProvider

---
+++ OnlineShop/Libs/OnlineShop.Libs.Data/Contracts/IEfDataProvider.cs
@@ -0,0 +1,18 @@
+﻿using OnlineShop.Libs.Models;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Libs.Data.Contracts
+{
+    public interface IEfDataProvider
+    {
+        IEfQuerable<Category> Categories { get; }
+
+        IEfQuerable<Product> Products { get; }
+
+        IEfQuerable<PhotoItem> PhotoItems { get; }
+
+        int SaveChanges();
+
+        Task<int> SaveChangesAsync();
+    }
+}
