3f414d0c351f2afdad29f4f06331c316c92c6b3a
Stoiko Neykov <stoikoneykov@gmail.com>
2017-03-10 16:13:16 +0200

    added EfQuerable GetEnumerator_Should Tasts

---
+++ OnlineShop/Tests/LibsTests/OnlineShop.Libs.Data.Tests/EfQuerableTasts/GetEnumerator_Should.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using NUnit.Framework;
+using OnlineShop.Libs.Data.Contracts;
+using OnlineShop.Libs.Data.Tests.Mocks;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace OnlineShop.Libs.Data.Tests.EfQuerableTasts
+{
+    [TestFixture]
+    public class GetEnumerator_Should
+    {
+        [Test]
+        public void Return_DbSet_GetAnumerator_Without_ChangeIt()
+        {
+            // Arange
+            var mockedEnumerator = new Mock<IEnumerator<DimmyClass>>();
+
+            var mockedDbSet = new Mock<IDbSet<DimmyClass>>();
+            mockedDbSet.Setup(x => x.GetEnumerator()).Returns(mockedEnumerator.Object);
+
+            var mockedEntryProvider = new Mock<IEfEntryProvider>();
+
+            var obj = new EfQuerable<DimmyClass>(mockedDbSet.Object, mockedEntryProvider.Object);
+
+            // Act
+            var result = obj.GetEnumerator();
+
+            // Assert
+            Assert.AreSame(mockedEnumerator.Object, result);
+        }
+    }
+}
