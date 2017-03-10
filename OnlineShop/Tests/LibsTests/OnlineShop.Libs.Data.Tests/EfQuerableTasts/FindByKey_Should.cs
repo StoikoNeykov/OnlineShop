523924063b4769e02b2acec6456a5a9ee0d3b29e
Stoiko Neykov <stoikoneykov@gmail.com>
2017-03-10 14:34:26 +0200

    Added EfQuerable FindByKey_Should Tasts

---
+++ OnlineShop/Tests/LibsTests/OnlineShop.Libs.Data.Tests/EfQuerableTasts/FindByKey_Should.cs
@@ -0,0 +1,36 @@
+﻿using Moq;
+using NUnit.Framework;
+using OnlineShop.Libs.Data.Contracts;
+using OnlineShop.Libs.Data.Tests.Mocks;
+using System.Data.Entity;
+
+namespace OnlineShop.Libs.Data.Tests.EfQuerableTasts
+{
+    [TestFixture]
+    public class FindByKey_Should
+    {
+        [TestCase(new object[] { 5, "randomsString" })]
+        [TestCase(new object[] { 0, "", 0, 8, "3cdcdsacda", "randomsString" })]
+        [TestCase(new object[] { "weqcewqc", 82, null, 5, "randomsString" })]
+        [TestCase(new object[] { 0 })]
+
+        public void CallOnce_DbSet_Find_WithSameArguments(object[] randomParams)
+        {
+            object[] actualParams = null;
+
+            // Arange
+            var mockedEntryProvider = new Mock<IEfEntryProvider>();
+
+            var mockedDbSet = new Mock<IDbSet<DimmyClass>>();
+            mockedDbSet.Setup(x => x.Find(It.IsAny<object[]>())).Callback((object[] x) => actualParams = x);
+
+            var obj = new EfQuerable<DimmyClass>(mockedDbSet.Object, mockedEntryProvider.Object);
+
+            // Act 
+            obj.FindByKey(randomParams);
+
+            // Assert
+            CollectionAssert.AreEquivalent(randomParams, actualParams);
+        }
+    }
+}
