6a2f16dd01484a0c9c82a2f8a59c1fcae5aa62fa
Stoiko Neykov <stoikoneykov@gmail.com>
2017-03-14 18:11:08 +0200

    added simple test to force build test dll

---
+++ OnlineShop/Tests/LibsTests/OnlineShop.Libs.Services.Tests/TemporaryClass.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace OnlineShop.Libs.Services.Tests
+{
+    [TestFixture]
+    public class TemporaryClass
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
