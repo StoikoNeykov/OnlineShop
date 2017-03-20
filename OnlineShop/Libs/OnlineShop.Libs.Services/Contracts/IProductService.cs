f8034d638316054c736d301284c2767561e02418
Stoiko Neykov <stoikoneykov@gmail.com>
2017-03-20 11:59:26 +0200

    aeed Product service

---
+++ OnlineShop/Libs/OnlineShop.Libs.Services/Contracts/IProductService.cs
@@ -0,0 +1,10 @@
+﻿using OnlineShop.Libs.Models;
+using System.Collections.Generic;
+
+namespace OnlineShop.Libs.Services.Contracts
+{
+    public interface IProductService : IService
+    {
+        IEnumerable<Product> GetProducts(int page, int pageSize = 10);
+    }
+}
---
+++ OnlineShop/Libs/OnlineShop.Libs.Services/ProductService.cs
@@ -0,0 +1,30 @@
+﻿using Bytes2you.Validation;
+using OnlineShop.Libs.Data.Contracts;
+using OnlineShop.Libs.Models;
+using OnlineShop.Libs.Services.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OnlineShop.Libs.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IEfQuerable<Product> products;
+
+        public ProductService(IEfDataProvider dataProvider)
+        {
+            Guard.WhenArgument(dataProvider, nameof(dataProvider)).IsNull().Throw();
+
+            this.products = dataProvider.Products;
+        }
+
+        public IEnumerable<Product> GetProducts(int page, int pageSize = 10)
+        {
+            return this.products
+                .GetAvailabe
+                .Skip(page * pageSize)
+                .Take(pageSize)
+                .ToList();
+        }
+    }
+}
