8070ff8fe90c57bab77692ff3df9a5c960056060
Stoiko Neykov <stoikoneykov@gmail.com>
2017-03-25 07:56:52 +0200

    removed bootstrap again and added some controllers

---
+++ OnlineShop/Clients/OnlineShop.Clients.MVC/Areas/Admin/Controllers/AdminHomeController.cs
@@ -0,0 +1,14 @@
+﻿using OnlineShop.Clients.MVC.Areas.Admin.Controllers.Abstraction;
+using System.Web.Mvc;
+
+namespace OnlineShop.Clients.MVC.Areas.Admin.Controllers
+{
+    public class AdminHomeController : AdminController
+    {
+        // GET: Admin/AdminHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ OnlineShop/Clients/OnlineShop.Clients.MVC/Areas/Admin/Controllers/AdminProductsController.cs
@@ -0,0 +1,14 @@
+﻿using OnlineShop.Clients.MVC.Areas.Admin.Controllers.Abstraction;
+using System.Web.Mvc;
+
+namespace OnlineShop.Clients.MVC.Areas.Admin.Controllers
+{
+    public class AdminProductsController : AdminController
+    {
+        // GET: Admin/AdminProducts
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}
