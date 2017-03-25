a290fdc1ca9a7d553b1cbc211ac81b5e734d7d15
Stoiko Neykov <stoikoneykov@gmail.com>
2017-03-25 07:41:35 +0200

    added admin area

---
+++ OnlineShop/Clients/OnlineShop.Clients.MVC/Areas/Admin/Controllers/Abstraction/AdminController.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Mvc;
+
+namespace OnlineShop.Clients.MVC.Areas.Admin.Controllers.Abstraction
+{
+    [Authorize(Roles ="admin")]
+    public abstract class AdminController : Controller
+    {
+    }
+}
